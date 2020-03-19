99fd7ff22f64345501a1040c6ba38a8de71c2f86
Visily <Michael4161@hotmail.com>
2020-03-18 21:18:51 -0600

    removed accidental ctrl+c from index.js

--- src/public/js/index.js
+++
@@ -1,9 +0,0 @@
-$('#form-id').on("submit", function(event){
-    event.preventDefault();
-    console.log("test")
-    // let data = $(this).serialize();
-
-    // $.post("/register",data ,function(data){
-    //     console.log(data);
-    // });
-});
