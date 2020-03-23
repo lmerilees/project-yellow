be9fb4a6988bf8fdd1bc82647fc069b048630e58
Visily <Michael4161@hotmail.com>
2020-03-23 01:37:52 -0600

    sidebar proj dyn add

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,209 +1,113 @@
-// global variables
-var userID;
-var rowcount = 0;
-
-
 $(document).ready(function () {
+    let User_Id = 0;
+    let project_name;
+
+    $.post("getUser",function(data){
+        console.log(data);
+        $("#username").append(data);
+        
+    });
+
+    $.post("getUserID",function(data){
+        User_Id = data.rows[0].user_id;
+        allProjects(User_Id);
+    });
+    // $.post("getProjectID",function(data){
+    //     User_Id = data.rows[0].user_id;
+    // });
 
     // this function handles the sidebar collapse
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
 
-    // this function will create append a new modal task button to the page when New Step is clicked
-    $('#newStep').on('click', function(event) {
-        event.preventDefault();
+    
 
-        rowcount = countRows();
-        console.log(rowcount);
+    // shows the modal for project name 
+    $("#new-project").on('click',() => {
+        // clear remaining data from modal
+        $("#modal-header").empty();
+        $("#append-body").empty();
 
-        var stepName = 'Step' + rowcount;
-        var userID = 1;
-        
-        $.ajax({
-            url: '/addStep',
-            method: 'POST',
-            async: true,
-            data: {stepName: stepName, userID: userID},
-            success: function(response){
-                // add new step to the page
-                addStep();
-                // keep row count up to date
-                countRows();
-            
-            },
-            error: function(response, status, err){
-                console.log(response);
-                console.log(status);
-                throw err
+        // append modal title and input field and show modal
+        $("#modal-header").append("New Project");
+        $("#append-body").append("<input type='text' id='project-input' class='form-control' placeholder='project name'> </input>");
+        $("#myModal").modal({show:true});
+    });
+
+    $("#modal-save").on('click', () =>{
+        // send project name to database
+        let projectName = $("#append-body #project-input").val().trim()
+        project_name = projectName;
+        $.post("project-add", {proj_name:projectName, user_id:User_Id}, function(data){
+            if(data == "added"){
+                        // add select to html
+                $('#append-project').append('<a href="#'+ projectName.split(' ').join('_')+'" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">'+ projectName +'</a>')
+                .append('<ul class="collapse list-unstyled" id="'+projectName.split(' ').join('_')+'"></ul>');
+                $('#append-project').children().last().append('<li></li>');
+                $('#append-project').children().last().children().last().append('<a href="#" onclick="selectProj(this);" id="'+projectName+'" >Select</a>');
+                $('#append-project').children().last().append('<li></li>');
+                $('#append-project').children().last().children().last().append('<a href="#" onclick="deleteProj(this);" id="'+projectName+'">Delete</a>');
+
+                // hide modal
+                $("#myModal").modal('hide');
+            }else{
+                alert(data);
             }
         });
+
+
     });
 
-    // add new subtask to modal-body when new task button is clicked
-    $(document).on('click', '#newTask' ,function() {
-        console.log("Task Clicked!");
+    $("#newStep").on('click',() => {
+        console.log("test");
 
-        $("#mymodal1").find(".modal-body").append('<div class="form-group">' +
-        '<label class="checkbox-inline"><input type="checkbox" value=""></label>' + 
-        '<input type="input" class="form-control-inline" id="task1" placeholder="Enter Task Name">' +
-        '</div>')
-        
-        
-         $('#tasks').append($(
-            '<div class="form-group">' +
-                '<label class="checkbox-inline"><input type="checkbox" value=""></label>' + 
-                '<input type="input" class="form-control-inline" id="task1" placeholder="Enter Task Name">' +
-            '</div>'))
+        // startup a modal
+        // $("#card-input").prepend();
     });
+ 
 
 
-    // this will auto-expand the modal body as items are appended
-    $('#modal').on('show.bs.modal', function () {
-        $(this).find('.modal-body').css({
-               width:'auto', //probably not needed
-               height:'auto', //probably not needed 
-               'max-height':'100%'
+
+    function allProjects(UserID){
+        $.post("allProjects", {userID:UserID}, function(data){
+            for(let i in data.rows){
+                let projectName = data.rows[i].project_name;
+
+                $('#append-project').append("<li id=side-"+projectName+"></li>")
+                $('#append-project').children().last().append('<a href="#'+ projectName.split(' ').join('_')+'" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">'+ projectName +'</a>')
+                .append('<ul class="collapse list-unstyled" id="'+projectName.split(' ').join('_')+'"></ul>');
+                $('#append-project').children().last().children().last().append('<li></li>');
+                $('#append-project').children().last().children().last().children().last().append('<a href="#" onclick="selectProj(this);" id="'+projectName+'" >Select</a>');
+                $('#append-project').children().last().children().last().append('<li></li>');
+                $('#append-project').children().last().children().last().children().last().append('<a href="#" onclick="deleteProj(this);" id="'+projectName+'">Delete</a>');
+            }
         });
-    });
+    }
 
 });
+function selectProj(project){
+    // server get data from projectid and User_id
+    $.post("getProjectData",function(data){
+        // insert data to title
 
-// update username on page load
-$(function updateUser(){
-    $.ajax({
-        method: "GET",
-        url: "/getuser",
-        async: true,
-        success: function(response){
-            $('pbody').append('<div id="username" style="text-align: center">' + response + '</div>');
-        },
-        error: function(xhr, status, error){
-            console.log(xhr);
-            console.log(status);
-        }
-    })
-})
-
-
-// get userid of current user
-function getUserID(){
-    $.ajax({
-        method: "GET",
-        url: "/getUserId",
-        async: true,
-        success: function(response){
-            var userID = response.rows[0].user_id;
-            console.log(userID);
-            return userID;
-        }
-    })
-    return userID;
+    });
+    $.post("getStepsData",function(data){
+        // insert data to body
+        
+    });
+    console.log(project.id);
 }
 
 
-// we need to keep count of the number of rows in our step table so we can create unique modals
-function countRows(){
 
-    // change this if we can figure out how to get userid
-    let userID = 1;
-    
-    $.ajax({
-        method: "POST",
-        url: "/getRowCount",
-        async: true,
-        dataType: "json",
-        data: {userID: userID},
-        success: callbackCountRows,
-        error: function(xhr, status, error){
-            //console.log(xhr);
-            console.log(status + " in getRowCount");
+
+function deleteProj(project){
+    $.post("deleteProject",{projectName:project.id},function(data){
+        // insert data to body
+        if(data == "deleted"){
+            $("#side-"+project.id).remove()
         }
-    })
-}
-
-
-function callbackCountRows(data){
-    rowcount = data;
-    console.log(rowcount);
-    return rowcount;
-}
-
-
-// populate users steps on page load
-$(function populateSteps(){
-
-    // this shouldn't be hardcoded if we have more than 1 user on same machine
-    let userID = 1;
-
-    $.ajax({
-        method: "POST",
-        url: '/populateSteps',
-        data: {userID: userID},
-        async: true,
-        success: function(results){
-
-            // iterate over steps stored in database and append to page
-            Object.keys(results).forEach(function(result){
-  
-                $('#steps').append($(
-                '<button type="button" class="btn btn-info btn-lg" style="padding: 5px" data-toggle="modal" data-target="#myModal'+ result.stepName + '">' + result.stepName + '</button>' +  
-                    '<div class="modal fade" id="myModal'+ result.stepName + '" role="dialog" style="padding: 2px">' + 
-                        '<div class="modal-dialog modal-sm">' + 
-                            '<div class="modal-content">' + 
-                                '<div class="modal-header">' + 
-                                    '<h4 class="modal-title">' + result.stepName + '</h4>' + 
-                                    '<button type="button" class="close" data-dismiss="modal">&times;</button>' + 
-                                '</div>' + 
-                                '<div class="modal-body">' +
-                                    '<div class="subtasks">' + 
-                                        '<div class="form-group">' +
-                                            '<label class="checkbox-inline"><input type="checkbox" value=""></label>' + 
-                                            '<input type="input" class="form-control-inline" id="task' + result.stepName + '" placeholder="Enter Task Name">' +
-                                        '</div>' + 
-                                    '</div>' +
-                                '</div>' + 
-                                '<div class="modal-footer">' + 
-                                    '<button type="button" class="btn btn-info btn-md" id="newTask">New Task</button>' +
-                                    '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
-                                '</div>' + 
-                            '</div>' + 
-                        '</div>' + 
-                    '</div>' +
-                '</div>'))
-                $('#steps').append(' ');
-            })
-        }
-    })
-})
-
-// append new step to the page
-function addStep(){
-    $('#steps').append($(
-        '<button type="button" class="btn btn-info btn-lg" style="padding: 5px" data-toggle="modal" data-target="#myModal'+ rowcount + '">Step ' + rowcount + '</button>' +  
-        '<div class="modal fade" id="myModal'+ rowcount + '" role="dialog" style="padding: 2px">' + 
-            '<div class="modal-dialog modal-sm">' + 
-                '<div class="modal-content">' + 
-                    '<div class="modal-header">' + 
-                        '<h4 class="modal-title">Step ' + rowcount + '</h4>' + 
-                        '<button type="button" class="close" data-dismiss="modal">&times;</button>' + 
-                    '</div>' + 
-                    '<div class="modal-body">' +
-                        '<div class="subtasks">' + 
-                            '<div class="form-group">' +
-                                '<label class="checkbox-inline"><input type="checkbox" value=""></label>' + 
-                                '<input type="input" class="form-control-inline" id="task' + rowcount + '" placeholder="Enter Task Name">' +
-                            '</div>' + 
-                        '</div>' +
-                    '</div>' + 
-                    '<div class="modal-footer">' + 
-                        '<button type="button" class="btn btn-info btn-md" id="newTask">New Task</button>' +
-                        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
-                    '</div>' + 
-                '</div>' + 
-            '</div>' + 
-        '</div>' +
-    '</div>'))
-    $('#steps').append(' ');
-    rowcount++
+        console.log(data);
+    });
 }
--- src/server/server.js
+++ src/server/server.js
@@ -22,6 +22,43 @@ app.get("/login", function(req,res){
     res.sendFile(path.resolve(__dirname, '../html',"login.html"))
 });
 
+app.post("/login",function(req, res){
+  pool.query("Select * from users WHERE username ='" + req.body.username+"'", function(err,result){
+    let hash = result.rows[0].password;
+    bcrypt.compare(req.body.password, hash, function(err, isMatch) {
+      if (err) {
+        throw err
+      } else if (!isMatch) {
+        console.log("Password doesn't match!")
+        res.send("Username or Password is incorrect!");
+      } else {
+        req.session.loggedin = true;
+        req.session.username = result.rows[0].username;
+        req.session.user_id = result.rows[0].id;
+        console.log("Password matches!");
+        res.redirect("index");
+      }
+    })
+  });
+
+
+
+  // get current session username
+app.get("/getUser", function(req, res){
+  res.send(req.session.username);
+});
+
+
+app.get("/index", function(req,res){
+    if(req.session.loggedin){
+      res.sendFile(path.resolve(__dirname, '../html',"index.html"))
+    }
+    // need to get data from database and send to html js 
+    // to display on page
+  });
+
+});
+
 app.get("/register", function(req,res){
   res.sendFile(path.resolve(__dirname, '../html',"register.html"))
 });
@@ -54,102 +91,128 @@ app.post("/register", function(req, res){
   });
 });
 
-app.post("/login",function(req, res){
-  pool.query("Select * from users WHERE username ='" + req.body.username+"'", function(err,result){
-    let hash = result.rows[0].password;
-    bcrypt.compare(req.body.password, hash, function(err, isMatch) {
-      if (err) {
-        throw err
-      } else if (!isMatch) {
-        console.log("Password doesn't match!")
-        res.send("Username or Password is incorrect!");
-      } else {
-        req.session.loggedin = true;
-        req.session.username = result.rows[0].username;
-        req.session.user_id = result.rows[0].id;
-        console.log("Password matches!");
-        res.redirect("index");
-      }
-    })
-  });
-
-  // get userID of current user
-  app.get("/getUserID", function(req, res){
-    let sql = "SELECT user_id FROM users WHERE username = '" + req.session.username + "'";
-    pool.query(sql, function(err, result){
-      if (err){
-        throw err;
-      }else{
-        res.jsonp(result)
-      }
-    });
-  })
-
-  // get current session username
-  app.get("/getuser", function(req, res){
-    res.send(req.session.username);
-  })
-
-
-  // insert new step into database
-  app.post("/addStep", function(req, res){
-  
-    let sql = "INSERT INTO steps (step_id, stepname, user_id) VALUES(DEFAULT, " + "'" + req.body.stepName + "'" + ", " + req.body.userID + ")";
-
-    pool.query(sql, (err, results) => {
-      console.log(sql);
-      if (err){
-        throw err
-      }
-      else{
-        res.jsonp(results)
-      }
-    })
-  })
-
-  // select all steps with current userid (/project id?)
-  app.post('/populateSteps', function(req, res){
-    const userID = req.body.userID;
-    let sql = "SELECT * FROM steps where user_ID = " + "'" + userID + "'";
-    pool.query(sql, (err, results) => {
-      console.log(sql);
-      if (err){
-        throw err
-      }
-      else{
-        res.jsonp(results.rows)
-      }
-    })
-  })
-  
-  // count rows in current step table
-  app.post('/getRowCount', function(req, res){
-    let userID = req.body.userID;
-    let sql = "SELECT COUNT(*) AS rowcount FROM steps WHERE user_ID = " + "'" + userID + "'";
-    console.log(sql);
-    pool.query(sql, (err, results) => {
-      if (err){
-        throw err
-      }
-      else{
-        res.jsonp(results.rows[0].rowcount)
-      }
-    })
-  })
-  
-
-  app.get("/index", function(req,res){
-    if(req.session.loggedin){
-      res.sendFile(path.resolve(__dirname, '../html',"index.html"))
+// CREATE TABLE projects(user_id int not null, project_id serial primary key not null, project_name VARCHAR(50) not null unique );
+app.post("/project-add", function(req,res){
+  console.log(req.body.proj_name)
+  console.log(req.body.user_id)
+  let sql = "INSERT INTO projects(user_id, project_name) VALUES ('"+req.body.user_id+"', '"+req.body.proj_name+"')"
+  pool.query(sql,function(err,result){
+    if(err){
+      res.send("Insert Error")
+    } else{
+      res.send("added");
     }
-    // need to get data from database and send to html js 
-    // to display on page
   });
+});
+
+
+
+// get userID of current user
+app.post("/getUserID", function(req, res){
+  let sql = "SELECT user_id FROM users WHERE username = '" + req.session.username + "'";
+  pool.query(sql, function(err, result){
+    if (err){
+      throw err;
+    }else{
+      res.jsonp(result)
+    }
+  });
+});
+
+app.post("/getProjectData", function(req,res){
 
 });
+app.post("/getStepsData", function(req,res){
+
+});
+
+app.post("/deleteProject", function(req,res){
+  let sql = "SELECT project_id FROM projects WHERE project_name = '" + req.body.projectName + "'"
+  pool.query(sql, function(err,result){
+    console.log(result.rows[0].project_id);
+    if(err){
+      res.send("err in project name");
+    }else{
+      sql = "DELETE FROM steps where project_id = " + result.rows[0].project_id
+      pool.query(sql, function(err){
+        if(err){
+          console.log("test");
+          res.send("deleted");
+          pool.query("DELETE FROM projects where project_id = " + result.rows[0].project_id,function(err){
+            if(err){
+              console.log("error in deleting project")
+            }
+          });
+        }else{
+          pool.query("DELETE FROM projects where project_id = " + result.rows[0].project_id, function(err){
+            if(err){
+              console.log("error in deleting project")
+            }
+          })
+          res.send("deleted");
+        }
+      });
+    }
+  });
+});
+
+app.post("/allProjects", function(req,res){
+  pool.query("SELECT * FROM projects WHERE user_id ='" + req.body.userID +"'", function(err, result){
+    if(err){
+      res.send("err");
+    }else{
+      res.send(result);
+    }
+  });
+});
+  // // insert new step into database
+  // app.post("/addStep", function(req, res){
+  
+  //   let sql = "INSERT INTO steps (step_id, stepname, user_id) VALUES(DEFAULT, " + "'" + req.body.stepName + "'" + ", " + req.body.userID + ")";
+
+  //   pool.query(sql, (err, results) => {
+  //     console.log(sql);
+  //     if (err){
+  //       throw err
+  //     }
+  //     else{
+  //       res.jsonp(results)
+  //     }
+  //   })
+  // })
+
+  // // select all steps with current userid (/project id?)
+  // app.post('/populateSteps', function(req, res){
+  //   const userID = req.body.userID;
+  //   let sql = "SELECT * FROM steps where user_ID = " + "'" + userID + "'";
+  //   pool.query(sql, (err, results) => {
+  //     console.log(sql);
+  //     if (err){
+  //       throw err
+  //     }
+  //     else{
+  //       res.jsonp(results.rows)
+  //     }
+  //   })
+  // })
+  
+  // // count rows in current step table
+  // app.post('/getRowCount', function(req, res){
+  //   let userID = req.body.userID;
+  //   let sql = "SELECT COUNT(*) AS rowcount FROM steps WHERE user_ID = " + "'" + userID + "'";
+  //   console.log(sql);
+  //   pool.query(sql, (err, results) => {
+  //     if (err){
+  //       throw err
+  //     }
+  //     else{
+  //       res.jsonp(results.rows[0].rowcount)
+  //     }
+  //   })
+  // })
+  
 
 https.createServer({
   key: fs.readFileSync( path.resolve( 'src/server/key.pem')),
   cert: fs.readFileSync(path.resolve('src/server/cert.pem'))
-}, app)
-.listen(8000)
+}, app).listen(8000)
